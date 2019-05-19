37118e04d3c194c805dfe15156688fa2ce32f4a4
TeoNaste <teonaste@gmail.com>
2019-05-19 20:16:29 +0300

    reverted filename

--- Labs/lab1/3-ProiectAgenda/ProiectAgenda/src/main/java/agenda/repository/classes/RepositoryContactFile.java
+++ Labs/lab1/3-ProiectAgenda/ProiectAgenda/src/main/java/agenda/repository/classes/RepositoryContactFile.java
@@ -15,7 +15,7 @@ import agenda.repository.interfaces.RepositoryContact;
 
 public class RepositoryContactFile implements RepositoryContact {
 
-	private static final String filename = "D:\\an3\\sem2\\vvss\\vvssgit\\Labs\\lab1\\3-ProiectAgenda\\ProiectAgenda\\files\\contacts.txt";
+	private static final String filename = "files\\contacts.txt";
 	private List<Contact> contacts;
 
 	public RepositoryContactFile() throws Exception {
